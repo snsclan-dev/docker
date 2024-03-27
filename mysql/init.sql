4293884314f5bf6d6c9f2e69268c674b78514a64
snsclan <snsclan.dev@gmail.com>
2024-03-27 18:16:04 +0900

    0327 0616

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,6 +1,7 @@
 -- CREATE SCHEMA `rhymix` ;
 CREATE SCHEMA `wordpress` ;
 
+-- USE mysql;
 -- CREATE USER 'snsdev'@'%' identified by 'ThdWo#Gus2452@S';
 -- GRANT ALL PRIVILEGES ON rhymix.* to 'snsdev'@'%';
 -- FLUSH PRIVILEGES;
